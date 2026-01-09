777a40332b160fed24ab106084518c9c5099489c
navinmonet <navin@ashmar.in>
2026-01-09 17:25:31 +0530

    string  in  javascript

---
+++ 4string.js
@@ -0,0 +1,31 @@
+
+// java script  string '' "";
+
+let  name ="navin"
+ let  repocount =4;
+  console.log(name+repocount);
+    console.log(`repo count   is  template string ${name}+${repocount}`);
+
+let strinTest="Navinkumar";
+ console.log(strinTest[1]);
+  console.log(strinTest);
+   console.log(strinTest.length);
+    console.log (strinTest.toLocaleUpperCase());
+     console.log(strinTest.charAt(3)); // i
+      console.log(strinTest.indexOf('N')); // o
+       const  newString = strinTest.substring(0,2); // NA
+        console.log(newString);
+         const slice_string = strinTest.slice(-5 ,4);
+          console.log(slice_string);
+          //  rev string 
+           const  strin_trim =" hites   "
+            console.log(strinTest);
+            console.log(strin_trim.trim()); // replace space 
+             const replace ="htps://nabbkoo.com";
+               console.log(replace.replace('com','co'));
+               console.log(replace.includes("htps"));
+               // string  convert array
+               let str = "Navin Kumar";
+               console.log(str.split(" "));
+               let text = "one two three four";
+console.log(text.split(" ", 2));
