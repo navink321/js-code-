ded67c393217c4c1a3f010f8ae24dc63af6b9af6
navinmonet <navin@ashmar.in>
2026-01-09 15:34:16 +0530

    conversion  of  javascript

---
+++ 3conversion.js
@@ -0,0 +1,23 @@
+let score="350abc";
+ console.log(typeof score);
+  let valueOFNumber=Number(score);
+   console.log(typeof valueOFNumber);
+    console.log(valueOFNumber); // 
+    //NAN  Invalid number in  javascript
+     let score2= null;
+    let  valueOFNumberScore2= Number(score2);
+     console.log(score2);
+     let score3= undefined;
+     let valueOFNumberundefined =  Number(score3);
+       console.log(valueOFNumberundefined);
+       // number conver esaly convert  but  string  not  convert out put  is nan 
+        let isBoolean =1;
+         console.log(typeof isBoolean);
+         let value_of_bloean = Boolean(isBoolean);
+          console.log(value_of_bloean);
+
+        let string_store=33;
+          console.log( typeof  string_store);
+        let  convert_string =  String(string_store);
+         console.log(typeof convert_string);
+         console.log(convert_string);
